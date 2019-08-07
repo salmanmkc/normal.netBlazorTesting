0459fdee7dcea7b3aa9d59be45d14a4527f8f65a
Salman Chishti <13schishti@gmail.com>
2019-08-07 11:11:07 +0100

    Add project files.

---
+++ App1/ProjectsObj.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    public class ProjectsObj
+    {
+        public ProjectsObj(string usertitle, string usersubtitle, string userdescription)
+        {
+            Title = usertitle;
+            Subtitle = usersubtitle;
+            Description = userdescription;
+        }
+
+        public ProjectsObj(string usertitle)
+        {
+            Title = usertitle;
+
+        }
+
+        public ProjectsObj(string usertitle, string usersubtitle)
+        {
+            Title = usertitle;
+            Subtitle = usersubtitle;
+        }
+
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                if (value != "")
+                {
+                    title = value;
+                }
+                else
+                {
+                    throw new Exception("Can't have an title");
+                }
+
+            }
+        }
+
+        private string subtitle;
+
+        public string Subtitle
+        {
+            get { return subtitle; }
+            set { subtitle = value; }
+        }
+
+        private string description;
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+
+    }
+}
